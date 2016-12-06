4a0b423450e59be1105b6e55b646b2b2f96dd8da
Paul Colomiets <paul@colomiets.name>
2016-12-06 18:47:06 +0200

    WIP: using sink instead of service

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+//! This is a collection of different connection pool implementations
+//!
+//! Note the API is still in flux.
+//!
+//! All connection pools work on a `futures::sink::Sink`. When you use this
+//! module for request-reply style options you should have something like
+//! `Sink<(Request, Sender<Reply>)>`.
+//!
+#[warn(missing_docs)]
 extern crate futures;
 extern crate abstract_ns;
 extern crate tokio_core;
-extern crate tokio_service;
 
-mod pool;
-
-pub use pool::ConnectionPool;
+pub mod uniform;
---
+++ src/uniform.rs
@@ -0,0 +1,110 @@
+//! This module provides and uniform connection pool implementation,
+//! which means we create a fixed number of connections for each IP/Port pair
+//! and distribute requests by round-robin
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::collections::VecDeque;
+
+use abstract_ns::Address;
+use tokio_core::reactor::Handle;
+use futures::{StartSend, Async, Future, BoxFuture, Poll};
+use futures::sink::{Sink};
+use futures::stream::{Stream, BoxStream};
+
+
+/// A simple uniform connection pool
+///
+/// This pool connects fixed number of connections
+/// to each IP/Port pair (if they are available) and distribute requests
+/// by round-robin
+///
+/// Note the pool has neither a buffer of it's own nor any internal tasks, so
+/// you are expected to use `Sink::buffer` and call `poll_complete` on every
+/// wake-up.
+pub struct Pool<S, E> {
+    address: BoxStream<Address, E>,
+    active: VecDeque<S>,
+    pending: VecDeque<BoxFuture<S, E>>,
+    config: Arc<Config>,
+    handle: Handle,
+}
+
+
+/// Configuration of the connection pool
+///
+/// Note default configuration doesn't make sense for most cases. Please
+/// tweak at least `Config::connections_per_address`.
+///
+/// Also make sure to use `eager_connections()` if you expect performance.
+pub struct Config {
+    connections_per_address: usize,
+    lazy_connections: bool,
+}
+
+impl Config {
+    /// Create a new config with default configuration
+    ///
+    /// Default configuration has `connections_per_address: 1` which is only
+    /// useful if you have synchronous workers and only one of them is bound
+    /// to every socket, or if there is a virtually infinite resources on the
+    /// other side (comparing to the number of requests we are going to do)
+    /// and good pipelining support.
+    pub fn new() -> Config {
+        Config {
+            connections_per_address: 1,
+            lazy_connections: true,
+        }
+    }
+
+    /// Establish connections and keep them open even if there are no requests.
+    ///
+    /// Lazy connections are nicer when you have mostly idle connection pool
+    /// and don't need sub-millisecond latency. The connection is established
+    /// only when there are no other connections that can serve a request.
+    ///
+    /// Lazy connections are enabled by default because it what makes most
+    /// sense if you have `HashMap<Hostname, Pool>` and this is how most
+    /// connections pools work in other languages.
+    pub fn eager_connections(&mut self) -> &mut Self {
+        self.lazy_connections = false;
+        self
+    }
+}
+
+impl<S, E> Pool<S, E>
+    where S: Sink,
+          <S as Sink>::SinkError: Into<E>,
+{
+    /// Create a connection pool
+    ///
+    /// This doesn't establish any connections even in eager mode. You need
+    /// to call `poll_complete` to start.
+    fn new(config: &Arc<Config>, handle: &Handle,
+           address: BoxStream<Address, E>, connect: Connect<S, E>)
+        -> Pool<S, E>
+    {
+        Pool {
+            address: address,
+            active: VecDeque::new(),
+            pending: VecDeque::new(),
+            config: config.clone(),
+            handle: handle.clone(),
+        }
+    }
+}
+
+impl<S, E> Sink for Pool<S, E>
+    where S: Sink<SinkError=E>
+{
+    type SinkItem = S::SinkItem;
+    type SinkError = E;
+    fn start_send(&mut self, item: Self::SinkItem)
+        -> StartSend<Self::SinkItem, Self::SinkError>
+    {
+        unimplemented!();
+    }
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError>
+    {
+        unimplemented!();
+    }
+}
