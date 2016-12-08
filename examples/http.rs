17486a45f87b201bdb2b5e848a2f3b30949221bd
Paul Colomiets <paul@colomiets.name>
2016-12-08 18:31:45 +0200

    Some tweaks to make example work (still doesn't)

--- examples/http.rs
+++ examples/http.rs
@@ -4,26 +4,35 @@ extern crate futures;
 extern crate abstract_ns;
 extern crate futures_cpupool;
 extern crate tokio_core;
+extern crate env_logger;
 extern crate ns_std_threaded;
 
+use std::env;
 use std::sync::Arc;
 
 use abstract_ns::Resolver;
 use futures::Future;
+use futures::stream::Stream;
 use futures::future::join_all;
 use futures::sink::Sink;
+use futures::sync::mpsc::channel;
 use tk_pool::uniform::{Pool, Config as PConfig};
 use tokio_core::net::TcpStream;
 use minihttp::client::{Proto, Config as HConfig, Client, Error};
 
 fn main() {
+    if let Err(_) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", "warn");
+    }
+    env_logger::init().unwrap();
+
     let mut lp = tokio_core::reactor::Core::new().unwrap();
     let h1 = lp.handle();
     let h2 = lp.handle();
 
     let ns = ns_std_threaded::ThreadedResolver::new(
         futures_cpupool::CpuPool::new(1));
-    let mut pool = Pool::new(&Arc::new(PConfig::new()),
+    let pool_impl = Pool::new(&Arc::new(PConfig::new()),
         &h1,
         ns.subscribe("httpbin.org:80"),
         move |addr| {
@@ -32,13 +41,25 @@ fn main() {
                 .map(|c| Proto::new(c, &Arc::new(HConfig::new())))
                 .map_err(Error::Io))
             as Box<Future<Item=_, Error=_>>
-        }).buffer(10);
+        });
+    let (mut pool, rx) = channel(10);
+    let fut = rx.map_err(|_| -> Error {
+        unimplemented!();
+    }).forward(pool_impl).map(|_| {}).map_err(|_| {});
+    lp.handle().spawn(fut);
     lp.run(futures::lazy(|| {
         join_all((0..10).map(move |_| {
-            Client::fetch_url(&mut pool, "http://httpbin.org/delay/5")
-            .map(|r| {
+            let (codec, receiver) = minihttp::client::buffered::Buffered::
+                get("http://httpbin.org/delay/5".parse().unwrap());
+            assert!(pool.start_send(codec).unwrap().is_ready());
+            receiver.map(|r| {
                 println!("Received {} bytes", r.body().unwrap().len())
             })
+
+            //pool.fetch_url(&mut pool, "http://httpbin.org/delay/5")
+            //.map(|r| {
+            //    println!("Received {} bytes", r.body().unwrap().len())
+            //})
         }))
     })).unwrap();
 }
