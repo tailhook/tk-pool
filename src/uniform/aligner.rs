e6c77c7681a25572006ace23cf837a09d326d855
Paul Colomiets <paul@colomiets.name>
2017-11-10 01:19:31 +0200

    Add tests to aligner, more full implementation

--- src/uniform/aligner.rs
+++ src/uniform/aligner.rs
@@ -1,5 +1,6 @@
 use std::u32;
-use std::collections::{HashSet, BTreeMap};
+use std::collections::{HashSet, HashMap, BTreeMap};
+use std::collections::btree_map::Entry::{Occupied};
 use std::net::SocketAddr;
 
 use rand::{thread_rng, sample};
@@ -7,6 +8,7 @@ use rand::{thread_rng, sample};
 
 pub(crate) struct Aligner {
     items: BTreeMap<u32, HashSet<SocketAddr>>,
+    addrs: HashMap<SocketAddr, u32>,
 }
 
 
@@ -14,18 +16,44 @@ impl Aligner {
     pub fn new() -> Aligner {
         Aligner {
             items: BTreeMap::new(),
+            addrs: HashMap::new(),
         }
     }
-    pub fn add<I: IntoIterator<Item=SocketAddr>>(&mut self, iter: I) {
-        let mut new: HashSet<_> = iter.into_iter().collect();
-        for (_, addrs) in &self.items {
-            for a in addrs {
-                new.remove(a);
+    pub fn update<N, O>(&mut self, new: N, old: O)
+        where N: IntoIterator<Item=SocketAddr>,
+              O: IntoIterator<Item=SocketAddr>,
+    {
+        {
+            let zero = self.items.entry(0).or_insert_with(HashSet::new);
+            for addr in new {
+                let n = *self.addrs.entry(addr).or_insert(0);
+                if n == 0 {
+                    zero.insert(addr);
+                }
             }
         }
-        self.items.entry(0)
-            .or_insert_with(HashSet::new)
-            .extend(&new);
+        for addr in old {
+            if let Some(n) = self.addrs.remove(&addr) {
+                match self.items.entry(n) {
+                    Occupied(mut o) => {
+                        o.get_mut().remove(&addr);
+                        if o.get().len() == 0 {
+                            o.remove_entry();
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        // in case we inserted it in the first line and did not delete yet
+        match self.items.entry(0) {
+            Occupied(o) => {
+                if o.get().len() == 0 {
+                    o.remove_entry();
+                }
+            }
+            _ => {}
+        }
     }
     pub fn get<F>(&mut self, limit: u32, blist: F) -> Option<SocketAddr>
         where F: Fn(SocketAddr) -> bool
@@ -52,6 +80,8 @@ impl Aligner {
             self.items.entry(num+1)
                 .or_insert_with(HashSet::new)
                 .insert(addr);
+            let old = self.addrs.insert(addr, num+1);
+            debug_assert_eq!(old, Some(num));
             return Some(addr);
         }
         return None;
@@ -71,18 +101,78 @@ mod test {
     #[test]
     fn normal() {
         let mut a = Aligner::new();
-        a.add(vec![addr(1), addr(2), addr(3)]);
+        a.update(vec![addr(1), addr(2), addr(3)], vec![]);
 
         let mut counter = HashMap::new();
         for _ in 0..30 {
-            println!("A {:?}", a.items);
-            *counter.entry(a.get(100, |x| false).unwrap()).or_insert(0) += 1;
+            *counter.entry(a.get(100, |_| false).unwrap()).or_insert(0) += 1;
         }
         assert_eq!(counter, vec![
             (addr(1), 10),
             (addr(2), 10),
             (addr(3), 10),
         ].into_iter().collect::<HashMap<_, _>>());
+    }
 
+    #[test]
+    fn blacklisting() {
+        let mut a = Aligner::new();
+        a.update(vec![addr(1), addr(2), addr(3)], vec![]);
+
+        let mut counter = HashMap::new();
+        for _ in 0..6 {
+            *counter.entry(a.get(100, |_| false).unwrap()).or_insert(0) += 1;
+        }
+        assert_eq!(counter, vec![
+            (addr(1), 2),
+            (addr(2), 2),
+            (addr(3), 2),
+        ].into_iter().collect::<HashMap<_, _>>());
+
+        let blist1 = &|x| x == addr(1);
+        for _ in 0..6 {
+            *counter.entry(a.get(100, blist1).unwrap()).or_insert(0) += 1;
+        }
+        assert_eq!(counter, vec![
+            (addr(1), 2),
+            (addr(2), 5),
+            (addr(3), 5),
+        ].into_iter().collect::<HashMap<_, _>>());
+
+        for _ in 0..9 {
+            *counter.entry(a.get(100, |_| false).unwrap()).or_insert(0) += 1;
+        }
+        assert_eq!(counter, vec![
+            (addr(1), 7),
+            (addr(2), 7),
+            (addr(3), 7),
+        ].into_iter().collect::<HashMap<_, _>>());
+    }
+
+    #[test]
+    fn update() {
+        let mut a = Aligner::new();
+        a.update(vec![addr(1), addr(2), addr(3)], vec![]);
+
+        let mut counter = HashMap::new();
+        for _ in 0..6 {
+            *counter.entry(a.get(100, |_| false).unwrap()).or_insert(0) += 1;
+        }
+        assert_eq!(counter, vec![
+            (addr(1), 2),
+            (addr(2), 2),
+            (addr(3), 2),
+        ].into_iter().collect::<HashMap<_, _>>());
+
+        a.update(vec![addr(4)], vec![addr(2)]);
+        for _ in 0..8 {
+            *counter.entry(a.get(100, |x| false).unwrap()).or_insert(0) += 1;
+        }
+        assert_eq!(counter, vec![
+            (addr(1), 4),
+            (addr(2), 2),  // still here because we count attempts not conns
+            (addr(3), 4),
+            (addr(4), 4),
+        ].into_iter().collect::<HashMap<_, _>>());
     }
 }
